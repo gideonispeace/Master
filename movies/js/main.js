667f4d9da07ebe03e7862afeb36864065f378119
gideonispeace <39224847+gideonispeace@users.noreply.github.com>
2020-08-03 21:56:36 -0500

    Update main.js

--- movies/js/main.js
+++ movies/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText){
-  axios.get('http://www.omdbapi.com?s='+searchText)
+  axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=60101044'+searchText)
     .then((response) => {
       console.log(response);
       let movies = response.data.Search;
